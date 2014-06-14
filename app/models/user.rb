06856ca108cc8a3dd178d6260b5114e7acf31ea2
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-06-14 19:43:21 +0200

    add validations to users & pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+
+  validates :username, presence: true, length: { minimum: 2, maximum: 32 }
 end
